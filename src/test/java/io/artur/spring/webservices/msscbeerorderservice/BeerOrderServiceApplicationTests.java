17d3462611489f7f3e0d87f57a6a08281d99c499
Artur Przychodzen <artur2p@gmail.com>
2021-10-27 12:00:04 +0200

    initial

---
+++ src/test/java/io/artur/spring/webservices/msscbeerorderservice/BeerOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.artur.spring.webservices.msscbeerorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
